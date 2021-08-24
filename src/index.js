502b58b582a626ef538b7e8c785aa9bd30ab02d6
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-08-24 15:04:36 -0400

    conditionally show error latitude or loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+// import SeasonDisplay from "./SeasonDisplay";
 
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
@@ -18,14 +18,25 @@ class App extends React.Component {
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({ lat: position.coords.latitude }); // if youre updating state you need to use set state
+        this.setState({ lat: position.coords.latitude, errorMessage: "" }); // if youre updating state you need to use setState!!
+        console.log(position);
       },
-      (err) => console.log(err)
+
+      (err) => {
+        console.log(err);
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render() {
-    return <div>Latitude:{this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading</div>;
   }
 }
 
