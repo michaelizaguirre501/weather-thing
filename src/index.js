477c7d4b37641f9d126749a057f2977d0236a3d6
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-08-24 11:45:48 -0400

    class componenet using constructor, super(props), instantiate  state initially to null, use geolocation to get user position, USES setState to change the state above to the property from our geo fetch, update dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return <div>Hello</div>;
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => position,
+//     (err) => console.log(err)
+//   );
+//   return <div>latitude</div>;
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude }); // if youre updating state you need to use set state
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude:{this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
