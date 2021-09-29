8eeaa1252b7a68a3923eeac093a67cc789bbc74f
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-29 11:55:55 -0400

    finished

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    icon: "sun",
+    color: "orange",
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    icon: "snowflake",
+    color: "lightblue",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, icon, color } = seasonConfig[season];
+  return (
+    <div>
+      <h1>
+        <i
+          class={`${icon} icon`}
+          style={{ color: { color }, fontSize: 100 }}
+        ></i>
+        {text}
+        <i
+          class={`${icon} icon`}
+          style={{ color: { color }, fontSize: 100 }}
+        ></i>
+      </h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
